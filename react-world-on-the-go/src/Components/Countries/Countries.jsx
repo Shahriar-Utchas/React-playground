04ab6e45f924b502c99d5665b80208746f280b8c
SHAHRIAR UTCHAS <123489314+Shahriar-Utchas@users.noreply.github.com>
2025-03-26 22:42:38 +0600

    shows countries info from api

--- react-world-on-the-go/src/Components/Countries/Countries.jsx
+++ react-world-on-the-go/src/Components/Countries/Countries.jsx
@@ -1,5 +1,6 @@
 import React, { use } from "react";
 import Country from "../Country/Country";
+import "./Countries.css";
 
 const Countries = ({ CountriesPromise }) => {
   const Countries = use(CountriesPromise);
@@ -7,9 +8,9 @@ const Countries = ({ CountriesPromise }) => {
   return (
     <div style={{ textAlign: "center" }}>
       <h1>Countries Info</h1>
-      <div style={{ display: "flex", flexWrap: "wrap", gap: "50px", justifyContent: "center" }}>
+      <div className="countries">
         {Countries.map((country) => (
-          <Country country={country}></Country>
+          <Country key={country.cca3} country={country}></Country>
         ))}
       </div>
     </div>
--- react-world-on-the-go/src/Components/Country/Country.jsx
+++ react-world-on-the-go/src/Components/Country/Country.jsx
@@ -1,17 +1,17 @@
 import React from "react";
+import "./Country.css";
 
 const Country = ({ country }) => {
-  return (
-    <div>
-      <h2>{country.name.common}</h2>
-      <img src={country.flags.png} alt={country.name.common} />
-      <p>Region: {country.region}</p>
-      <p>Capital: {country.capital}</p>
-      <p>Population: {country.population}</p>
-      <p>Area: {country.area}</p>
-      <p>Timezones: {country.timezones}</p>
+return (
+    <div className="country">
+        <h2>{country.name.common}</h2>
+        <img src={country.flags.png} alt={country.name.common} width="60%" />
+        <p>Capital: {country.capital}</p>
+        <p>Population: {country.population}</p>
+        <p> Independence: {country.independent ? 'Free' : 'Not free'  }</p>
+        <button>Not Visited</button>
     </div>
-  );
+);
 };
 
 export default Country;
